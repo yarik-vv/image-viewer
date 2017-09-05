050fa69a46159ccbd953577bc6bd1213058bc613
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-05 14:16:33 +0300

    create module for load images with API

--- source/build.js
+++ source/build.js
@@ -2,5 +2,17 @@
 
 import scss from './scss/index.scss';
 import AJAXrequest from './request';
+import loadImages from './loadImages';
 
-
+let images = null;
+AJAXrequest('https://unsplash.it/list', 'GET').then(
+  function(result) {
+    images = JSON.parse(result);
+    images.splice(0, 555);
+    loadImages(images, 0);
+    console.log(images)
+  },
+  function(error) {
+    console.log(error);
+  }
+);
